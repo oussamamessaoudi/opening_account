b2f324444f46e3329a1139b215827827d51568ad
Oussama MESSAOUDI <oussm.messaoudi@gmail.com>
2024-03-01 21:38:29 -0500

    Initial commit

---
+++ src/main/java/me/oussamamessaoudi/opening_account/OpeningAccountApplication.java
@@ -0,0 +1,13 @@
+package me.oussamamessaoudi.opening_account;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpeningAccountApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OpeningAccountApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/oussamamessaoudi/opening_account/OpeningAccountApplicationTests.java
@@ -0,0 +1,13 @@
+package me.oussamamessaoudi.opening_account;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpeningAccountApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
