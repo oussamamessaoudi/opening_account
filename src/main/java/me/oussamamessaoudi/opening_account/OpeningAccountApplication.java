409161caf934265df99ff1636143b81868cd3a2d
Oussama MESSAOUDI <oussm.messaoudi@gmail.com>
2024-03-02 09:46:11 -0500

    Adding coverage in PR

--- src/main/java/me/oussamamessaoudi/opening_account/OpeningAccountApplication.java
+++ src/main/java/me/oussamamessaoudi/opening_account/OpeningAccountApplication.java
@@ -9,5 +9,15 @@ public class OpeningAccountApplication {
     public static void main(String[] args) {
         SpringApplication.run(OpeningAccountApplication.class, args);
     }
+    public boolean isPalindrome(String inputString) {
+        if (inputString.length() == 0) {
+            return true;
+        } else {
+            char firstChar = inputString.charAt(0);
+            char lastChar = inputString.charAt(inputString.length() - 1);
+            String mid = inputString.substring(1, inputString.length() - 1);
+            return (firstChar == lastChar) && isPalindrome(mid);
+        }
+    }
 
 }
--- src/test/java/me/oussamamessaoudi/opening_account/OpeningAccountApplicationTests.java
+++ src/test/java/me/oussamamessaoudi/opening_account/OpeningAccountApplicationTests.java
@@ -1,13 +1,23 @@
 package me.oussamamessaoudi.opening_account;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 class OpeningAccountApplicationTests {
 
     @Test
     void contextLoads() {
+        assertTrue(true);
+    }
+
+    @Test
+    public void whenEmptyString_thenAccept() {
+        var palindromeTester = new OpeningAccountApplication();
+        assertTrue(palindromeTester.isPalindrome(""));
     }
 
 }
