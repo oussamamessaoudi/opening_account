902f73db80961ebb8504ba0b8e8ddf09a5f36d2e
Oussama MESSAOUDI <oussm.messaoudi@gmail.com>
2024-03-04 21:00:24 -0500

    Adding cucumber

--- src/main/java/me/oussamamessaoudi/openingaccount/application/domain/model/NewAccountCreationDTO.java
+++ src/main/java/me/oussamamessaoudi/openingaccount/application/domain/model/NewAccountCreationDTO.java
@@ -1,11 +1,15 @@
 package me.oussamamessaoudi.openingaccount.application.domain.model;
 
 import java.math.BigDecimal;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class NewAccountCreationDTO {
 
   private long customerId;
--- src/main/java/me/oussamamessaoudi/openingaccount/application/exception/ProblemRest.java
+++ src/main/java/me/oussamamessaoudi/openingaccount/application/exception/ProblemRest.java
@@ -1,11 +1,15 @@
 package me.oussamamessaoudi.openingaccount.application.exception;
 
 import java.util.Map;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class ProblemRest {
   private String codeError;
   private String messageError;
